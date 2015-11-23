0ef12a2160ef364e0b7a36db5dcfb62e908cecba
Drew Clauson <aclauson@gmail.com>
2015-11-23 14:00:06 -0500

    additional fixes

--- DependencyInjection/Compiler/RegisterEventListenersPass.php
+++ DependencyInjection/Compiler/RegisterEventListenersPass.php
@@ -8,9 +8,20 @@
 
 namespace Elytus\LimoncelloBundle\DependencyInjection;
 
+use Elytus\LimoncelloBundle\Integration\SymfonyIntegration;
+use Sensio\Bundle\FrameworkExtraBundle\EventListener\ControllerListener;
+use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+
 class RegisterEventListenersPass implements CompilerPassInterface
 {
-    public function process(ContainerBuilder $container){
-        $container->getDefinition('json_api.controller_listener');
+    public function process(ContainerBuilder $container)
+    {
+        $container->setParameter('json_api.controller_listener.class', ControllerListener::class);
+        $container->setParameter('json_api.symfony_integration.class', SymfonyIntegration::class);
+
+        $container->register('json_api.controller_listener', '%json_api.controller_listener.class%');
+        $container->register('json_api.symfony_integration', '%json_api.symfony_integration.class%')
+            ->addMethodCall('setContainer', ['@service_container']);
     }
 }
--- ElytusLimoncelloBundle.php
+++ ElytusLimoncelloBundle.php
@@ -2,7 +2,9 @@
 
 namespace Elytus\LimoncelloBundle;
 
+use Elytus\LimoncelloBundle\DependencyInjection\RegisterEventListenersPass;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
 
 class ElytusLimoncelloBundle extends Bundle
 {
